b275aabad7a592ba717d5bbad7539bf59dd2ae1d
Jarod Smith <jarodms@gmail.com>
2019-12-27 11:43:41 -0600

    add more tests from tutorial to check clicks

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -4,6 +4,7 @@ import { IonicModule } from '@ionic/angular';
 import { Tab1Page } from './tab1.page';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from '../reducers/counter.reducer';
+import { By } from '@angular/platform-browser';
 
 describe('Tab1Page', () => {
   let component: Tab1Page;
@@ -26,4 +27,44 @@ describe('Tab1Page', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should increment the counter value when increment is clicked', () => {
+    clickByCSS('#increment');
+ 
+    expect(getCounterText()).toBe(
+      'Current Count: 1'
+    );
+  });
+
+  it('should decrement the counter value when decrement is clicked', () => {
+    clickByCSS('#decrement');
+ 
+    expect(getCounterText()).toBe(
+      'Current Count: -1'
+    );
+  }); 
+
+  it('should reset the counter value when reset is clicked', () => {
+    clickByCSS('#increment');
+    clickByCSS('#reset');
+ 
+    expect(getCounterText()).toBe(
+      'Current Count: 0'
+    );
+  });
+
+
+  // Util functions used for tests
+  function clickByCSS(selector: string) {
+    const debugElement = fixture.debugElement.query(By.css(selector));
+    const el: HTMLElement = debugElement.nativeElement;
+    el.click();
+    fixture.detectChanges();
+  }
+ 
+  function getCounterText() {
+    const compiled = fixture.debugElement.nativeElement;
+    return compiled.querySelector('div').textContent;
+  }
+  
 });
