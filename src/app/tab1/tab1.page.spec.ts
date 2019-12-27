e04159c37e2a5cae2455ffaedcec597060ecaefb
Jarod Smith <jarodms@gmail.com>
2019-12-27 11:36:48 -0600

    fix test

--- src/app/tab1/tab1.page.spec.ts
+++ src/app/tab1/tab1.page.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { Tab1Page } from './tab1.page';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from '../reducers/counter.reducer';
 
 describe('Tab1Page', () => {
   let component: Tab1Page;
@@ -10,7 +12,10 @@ describe('Tab1Page', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Tab1Page],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        StoreModule.forRoot({ count: counterReducer })
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab1Page);
